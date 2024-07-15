aa54bca2497216d0a182bd196f5a38fe30131700
John <jaricaya@gmail.com>
2024-07-15 03:08:33 -0700

    completed lab with Advanced Deliverables

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({plant, onToggleSoldOut, onUpdatePrice, onDeletePlant}) {
+  const [isSoldOut, setIsSoldOut] = useState(plant.isSoldOut)
+  //Advanced deliverable
+  const [newPrice, setNewPrice] = useState(plant.price.toString())
+
+  const handleSoldOutToggle = () => {
+    setIsSoldOut((prevIsSoldOut) => !prevIsSoldOut)
+    onToggleSoldOut(plant.id, !isSoldOut)
+  }
+  const handlePriceChange = (e) => {
+    setNewPrice(e.target.value);
+  }
+
+  const handlePriceUpdate = () => {
+    const updatedPrice = parseFloat(newPrice)
+    onUpdatePrice(plant.id, updatedPrice)
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      <input 
+        type = "number"
+        value = {newPrice}
+        onChange = {handlePriceChange}
+        placeholder= "Update Price"
+        >
+        </input>
+      <button onClick = {handlePriceUpdate}>Update Price</button>
+        <button onClick = {handleSoldOutToggle}>
+          {isSoldOut ? "Out of Stock" : "In Stock" }
+        </button>
+        <button onClick={() => onDeletePlant(plant.id)}> Delete Plant</button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,81 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+// import { setFiles } from "@testing-library/user-event/dist/cjs/utils/index.js";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then ((r) => r.json())
+    .then(setPlants)
+  }, [])
+
+  const handleAddPlant = (newPlant) => {
+    setPlants((plants) => [...plants, newPlant])
+  }
+
+  function handleUpdatePrice( id, newPrice) {
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ price: newPrice }),
+    })
+      .then((r) => r.json())
+      .then((updatedPlant) => {
+        console.log(updatedPlant)
+        const updatedPlants = plants.map((plant) => 
+          plant.id === id ? {...plant, price: updatedPlant.price } : plant
+      )
+        setPlants(updatedPlants)
+      })
+  }
+
+  const handleToggleSoldOut = (id, isSoldOut) => {
+    fetch (`http://localhost:6001/plants/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ isSoldOut: !isSoldOut }),
+    })
+    .then((r) => r.json())
+    .then((updatedPlant) => {
+      const updatedPlants = plants.map((plant) => 
+        plant.id === id ? {...plant, isSoldOut} : plant 
+      )
+        setPlants(updatedPlants)
+    })
+  }
+//Advanced Deliverable
+  const handleDeletePlant = (id) => {
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        const updatedPlants = plants.filter((plant) => plant.id !== id)
+        setPlants(updatedPlants)
+      })
+  }
+
+  const displayedPlants = plants.filter((plant) => 
+    plant.name ? plant.name.toLowerCase().includes(search.toLowerCase()) : false
+)
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant = {handleAddPlant}/>
+      <Search  setSearch = {setSearch} />
+      <PlantList 
+        plants = {displayedPlants} 
+        onToggleSoldOut={handleToggleSoldOut}
+        onUpdatePrice = {handleUpdatePrice}
+        onDeletePlant = {handleDeletePlant}/>
     </main>
   );
 }
